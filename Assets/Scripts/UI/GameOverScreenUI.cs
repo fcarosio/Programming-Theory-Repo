fb1a842917e3e8b5db6c92886a189c22454a2500
fcarosio <62181802+fcarosio@users.noreply.github.com>
2022-07-18 23:13:24 +0200

    Final adjustments

--- Assets/Scripts/UI/GameOverScreenUI.cs
+++ Assets/Scripts/UI/GameOverScreenUI.cs
@@ -2,24 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreenUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI winner;
+    [SerializeField] Button restartGame;
 
     // Start is called before the first frame update
     void Start()
     {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        restartGame.onClick.AddListener(RestartGame);
     }
 
     public void SetWinner(GameManager.PlayerData playerData)
     {
         gameObject.SetActive(true);
         winner.text = playerData.GetName() + " wins!";
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
